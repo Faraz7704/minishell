423b3dc0c7b192191018b6d9104c4eff2a6af588
Syimyk Zhakypov <szhakypo@lab1r1s11.42abudhabi.ae>
2022-08-14 19:29:54 +0400

    add header

---
+++ src/init.c
@@ -0,0 +1,10 @@
+#include "../include/minishell.h"
+
+int	init_env(t_list **anv, char **env)
+{
+	char	**tmp;
+	int		len;
+	printf("%s\n", env);
+
+	return();
+}
---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "../include/minishell.h"
+
+void parsing(char **av, char **env)
+{
+	
+}
+
+int main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	t_list	envv;
+	char *s;
+	
+	//init_env(&env, &envv);
+	//while (1)
+	//{
+	//	s = readline("Faraz GAY $ ");
+	//}
+	return (0);
+}
