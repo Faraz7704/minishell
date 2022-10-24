8efd49681bdce8875c36485bff09929df6669998
faraz7704 <faraz7704@gmail.com>
2022-10-24 15:41:57 +0400

    few expansion improvements

--- sources/parser/expansion.c
+++ sources/parser/expansion.c
@@ -6,60 +6,39 @@
 /*   By: fkhan <fkhan@student.42abudhabi.ae>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 15:59:56 by fkhan             #+#    #+#             */
-/*   Updated: 2022/10/23 20:45:13 by fkhan            ###   ########.fr       */
+/*   Updated: 2022/10/24 15:41:22 by fkhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-char	*parsekey(char *q, char *eq, t_env *env)
+char	*parsekey(char *q, char *eq)
 {
-	char	*s;
+	int		i;
 	char	*key;
-	int		len;
-	t_list	*keyvalue;
 
-	s = q;
-	len = 0;
-	while (s < eq && ft_isalnum(*s))
-	{
-		len++;
-		s++;
-	}
-	if (!len)
+	i = 0;
+	while (&q[i] < eq && ft_isalnum(q[i]))
+		i++;
+	if (!i)
 		return (NULL);
-	key = ft_strldup(q, len + 1);
+	key = ft_strldup(q, i + 1);
 	if (!key)
 		print_error("malloc error\n");
-	free(key);
-	return ((t_km *)keyvalue->content);
+	return (key);
 }
 
 t_km	*expansion(char *q, char *eq, t_env *env)
 {
-	char	*s;
 	char	*key;
-	int		len;
 	t_list	*keyvalue;
 
-	s = q;
-	len = 0;
-	while (s < eq && ft_isalnum(*s))
-	{
-		len++;
-		s++;
-	}
-	if (!len)
-		return (NULL);
-	key = ft_strldup(q, len + 1);
+	key = parsekey(q, eq);
 	if (!key)
-		print_error("syntax");
-	keyvalue = find_keymap_key(env->kms, key);
-	if (!keyvalue)
-	{
-		free(key);
 		return (NULL);
-	}
+	keyvalue = find_keymap_key(env->kms, key);
 	free(key);
+	if (!keyvalue)
+		return (NULL);
 	return ((t_km *)keyvalue->content);
 }
